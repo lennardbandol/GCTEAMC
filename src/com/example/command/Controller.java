97959c09429541fc1f3cec4288302ca383d46f4f
AndrewKLeech <andrewkennethleech@gmail.com>
2017-04-27 19:26:17 +0900

    fix merge conflict

--- src/com/example/command/Controller.java
+++ src/com/example/command/Controller.java
@@ -1,60 +1,3 @@
-<<<<<<< HEAD
-package com.example.command;
-
-import java.util.ArrayList;
-
-import com.example.business.Room;
-import com.example.dao.RoomDao;
-import com.example.dao.UserDao;
-import com.example.exceptions.DaoException;
-public class Controller {
-	
-	UserDao userdao = new UserDao();
-	RoomDao roomdao = new RoomDao();
-	
-	public boolean login(String uName, String pWord){
-		try {			
-			System.out.println("Try login");
-			userdao.login(uName, pWord);
-			return true;
-		} 
-		catch (DaoException e) {
-			System.out.println("fails login");
-			e.printStackTrace();
-			return false;
-		}
-	}//End login
-	
-	public boolean logout(){
-		return true;
-	}//End logout
-	
-	public boolean register(String regUsername, String regPassword, String regConfirmPassword, String registerEmail, String regDOB, String regContactNumber){
-		try {			
-			userdao.register(regUsername, regPassword, regConfirmPassword, registerEmail, regDOB, regContactNumber);
-			return true;
-		} 
-		catch (DaoException e) {
-			e.printStackTrace();
-			return false;
-		}
-	}//End register
-	
-	public ArrayList<Room> getRoom(String type,boolean smoking, float priceMin,float priceMax, int noOfPeople)
-	{
-		try{
-			System.out.println("Try Search");
-			return roomdao.getRoom(type,smoking,priceMin,priceMax,noOfPeople);
-		}
-		catch(DaoException e)
-		{
-			System.out.println("Fail to search");
-			e.printStackTrace();
-			return null;
-		}
-	}
-}
-=======
 package com.example.command;
 
 import com.example.dao.UserDao;
@@ -89,5 +32,18 @@ public class Controller {
 			return false;
 		}
 	}//End login
+	
+	public ArrayList<Room> getRoom(String type,boolean smoking, float priceMin,float priceMax, int noOfPeople)
+	{
+		try{
+			System.out.println("Try Search");
+			return roomdao.getRoom(type,smoking,priceMin,priceMax,noOfPeople);
+		}
+		catch(DaoException e)
+		{
+			System.out.println("Fail to search");
+			e.printStackTrace();
+			return null;
+		}
+	}
 }
->>>>>>> branch 'master' of https://github.com/AndrewKLeech/GCTEAMC
